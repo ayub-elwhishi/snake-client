aa8606ffd14b2f823b4c1b46b4746e9985dd4733
ayub <ayubyubster@gmail.com>
2023-07-21 09:42:48 +0000

    organized client handling into its own file

--- play.js
+++ play.js
@@ -1,24 +1,6 @@
-const net = require("net");
+const client = require("./client");
 
 // establishes a connection with the game server
-const connect = function () {
-	const conn = net.createConnection({
-		host: "10.0.2.15", // IP address here,
-		port: 50541, // PORT number here,
-	});
-
-	// interpret incoming data as text
-	conn.setEncoding("utf8");
-
-	conn.on("data", (data) => {
-		console.log("Server says: ", data);
-	});
-
-	conn.on("connect", () => {
-		console.log("Connected.");
-	});
-	return conn;
-};
 
 console.log("Connecting...");
-connect();
+client.connect();
