ea96faf0938b74ea22dd16e9d2db5bc97ccd548c
ayub <ayubyubster@gmail.com>
2023-07-21 13:10:40 +0000

    added readme

--- client.js
+++ client.js
@@ -1,25 +1,24 @@
 const net = require("net");
 const constants = require("./constants");
 
-const connect = function () {
-	const conn = net.createConnection({
-		host: constants.IP, // IP address here,
-		port: constants.PORT, // PORT number here,
-	});
+const connect = function() {
+  const conn = net.createConnection({
+    host: constants.IP, // IP address here,
+    port: constants.PORT, // PORT number here,
+  });
 
-	// interpret incoming data as text
-	conn.setEncoding("utf8");
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
 
-	conn.on("data", (data) => {
-		console.log("Server says: ", data);
-	});
+  conn.on("data", (data) => {
+    console.log("Server says: ", data);
+  });
 
-	conn.on("connect", () => {
-		console.log("Connected.");
-		conn.write("Name: AE")
-		//conn.write("Move: up")
-	});
-	return conn;
+  conn.on("connect", () => {
+    console.log("Connected.");
+    conn.write("Name: AE");
+  });
+  return conn;
 };
 
-module.exports = {connect}
+module.exports = {connect};
--- input.js
+++ input.js
@@ -1,29 +1,27 @@
-const constants = require("./constants")
+const constants = require("./constants");
 
-let connection = undefined
+let connection = undefined;
 
-const handleUserInput = function (key) {
-	if (key === "\u0003") {
-		process.exit();
-	}
-	else if(constants.MOVEMENT_KEYS[key] && connection)
-	{
-		connection.write(constants.MOVEMENT_KEYS[key])	
-	}
-	if (key === 'k') {
-		connection.write("Say: Hello!")
-	}
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  } else if (constants.MOVEMENT_KEYS[key] && connection) {
+    connection.write(constants.MOVEMENT_KEYS[key]);
+  }
+  if (key === 'k') {
+    connection.write("Say: Hello!");
+  }
 };
 
-const setupInput = function (conn) {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding("utf8");
-	stdin.resume();
-	stdin.on("data", handleUserInput);
-	if (conn) connection = conn;
-	return stdin;
+const setupInput = function(conn) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  if (conn) connection = conn;
+  return stdin;
 };
 
 
-module.exports = {setupInput}
+module.exports = {setupInput};
