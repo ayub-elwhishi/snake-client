76cb1ea364c374aeebeb71eece55ac68fa9ed38d
ayub <ayubyubster@gmail.com>
2023-07-21 10:31:50 +0000

    organized input handling into its own file

--- play.js
+++ play.js
@@ -1,23 +1,8 @@
 const client = require("./client");
+const input = require("./input");
 
 // establishes a connection with the game server
 
 console.log("Connecting...");
-client.connect();
-
-const handleUserInput = function (key) {
-	if (key === "\u0003") {
-		process.exit();
-	}
-};
-
-const setupInput = function () {
-	const stdin = process.stdin;
-	stdin.setRawMode(true);
-	stdin.setEncoding("utf8");
-	stdin.resume();
-	stdin.on("data", handleUserInput);
-	return stdin;
-};
-
-const std = setupInput();
+const conn = client.connect();
+const stdin = input.setupInput();
